bb523813961670c6e62142d2363ce2cb60a1f735
Rei <33325105+Eilaluth@users.noreply.github.com>
2021-11-02 18:24:55 +0700

    Update mocha.js

--- mocha.js
+++ mocha.js
@@ -7,8 +7,7 @@ const client = new Client({disableMentions: 'everyone'});
 client.once("ready", () => {
     console.log(`Online as ${client.user.tag}`);
 
-    let scheduledMessage = new cron.CronJob('0 */2 * * *', () => {
-        // bumping every 2 hours
+    let scheduledMessage = new cron.CronJob('0 0-23/2 * * *', () => { // bumping every 2 hours, you can also use 0 */2 * * * if this one is error
         const guild = client.guilds.cache.get(config.guild); // your guild id
         const channel = guild.channels.cache.get(config.channel); // your guild channel id
         channel.send(`!d bump`);
