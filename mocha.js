33fb48cf88090382529571b0636a6b2e62282b0c
Reidho Satria <33325105+elliottophellia@users.noreply.github.com>
2022-11-12 02:05:47 -0800

    update time problem

--- mocha.js
+++ mocha.js
@@ -8,13 +8,27 @@ client.on('ready', async () => {
     console.log(figlet.textSync('Mocha', { horizontalLayout: 'full' }));
     console.log(` Logged in as ${client.user.tag} at ${new Date().toLocaleString()}`);
     console.log(` From now on, I will be bumping Disbord every 2 hours!`);
-    cron.schedule('0 */2 * * *', () => {
+    
+    //-----------------------------------------------------------------------------------------------------------------------
+    cron.schedule('0 */2 * * *', () => { // if somethings goes wrong you can try using this cron : 0 0-23/2 * *
         client.guilds.cache.get(config.bump.server).channels.cache.get(config.bump.channel).sendSlash('302050872383242240', 'bump');
         console.log(' Bumping Disbord - ' + new Date().toLocaleString() + ' - ' + client.user.tag);
     }, {
         scheduled: true,
         timezone: "Asia/Jakarta"
     });
+    //-----------------------------------------------------------------------------------------------------------------------
+    // or you can comment code above and uncomment this code :
+    //-----------------------------------------------------------------------------------------------------------------------
+    /*
+    
+    setInterval(() => {
+        client.guilds.cache.get(config.bump.server).channels.cache.get(config.bump.channel).sendSlash('302050872383242240', 'bump');
+        console.log(' Bumping Disbord - ' + new Date().toLocaleString() + ' - ' + client.user.tag);
+    }, 7200000); // time written in milisecond so 7200000 equal to 2 hours
+    
+    */
+    //-----------------------------------------------------------------------------------------------------------------------
 })
 
 client.login(config.token);
